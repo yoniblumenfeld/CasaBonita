2221453ebaf219448e778319eb9711a864bd1bc4
yovelcohen <yovell04@gmail.com>
2020-04-03 22:59:44 +0300

    first commit

---
+++ CaseBonita/Services/PlaylistUploader/Handlers/Spotify.py
@@ -0,0 +1,15 @@
+class SpotifyUploader:
+
+    def __init__(self, service, name, user_id):
+        self.service = service
+        self.name = name
+        self.user_id = user_id
+
+    def _create_playlist(self, tracks=None):
+        if tracks is None:
+            tracks = []
+
+        playlist = self.service.user_playlist_create(self.user_id,
+                                                     self.name)
+        self.service.user_playlist_replace_tracks(self.user_id,
+                                                  playlist['id'], tracks)
