5bc7b6fdbd4fb8e43fb3103d8905216e2242a9d7
Yoni Blumenfeld <yonei06@gmail.com>
2020-04-09 02:09:47 +0300

    possible bug fix (:

--- CaseBonita/Misc/Yoni/send.py
+++ CaseBonita/Misc/Yoni/send.py
@@ -1,17 +1,15 @@
 import json
 
 from CaseBonita.Data.Consts import PlatformName
-from CaseBonita.Infrastructure.Consts import RABBIT_MQ_HOST, EntityNames, Actions
-from CaseBonita.Infrastructure.Messaging.Channel.Factory import ChannelFactory
+from CaseBonita.Infrastructure.Consts import EntityNames, Actions
 from CaseBonita.Infrastructure.Messaging.Topic.Factory import TopicFactory
 
 if __name__ == '__main__':
     entity_name = EntityNames.PLAYLIST_DOWNLOADER
     action = Actions.REQUESTED
-    channel = ChannelFactory.get_channel_handler(entity_name, action)
     topic = TopicFactory.get_topic(entity_name, action)
     msg = {
-        'source_url': "https://music.apple.com/il/playlist/shiras-18th-birthday/pl.u-9N9L24LIx3DZ3K0",
+        'source_url': "https://music.apple.com/us/playlist/melvins-deep-cuts/pl.adc8f720731842f8ac2eaf6d00ac38bd?app=itunes",
         'source_platform': PlatformName.APPLE_MUSIC
     }
     topic.publish(msg)
