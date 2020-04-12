9c29f94296d3bbdf0c8496447f5fbb8619865ce2
yovelcohen <yovell04@gmail.com>
2020-04-12 20:52:22 +0300

    spotify handler using spotify

--- CaseBonita/Data/Consts.py
+++ CaseBonita/Data/Consts.py
@@ -2,3 +2,27 @@ class PlatformName(object):
     SPOTIFY = 'spotify'
     APPLE_MUSIC = 'apple_music'
     DEEZER = 'deezer'
+
+
+CHROMEDRIVER = "/home/yovel/PycharmProjects/chromedriver"
+
+APPLEMUSIC_LOGIN_PAGE = 'https://music.apple.com/us/artist/login/439834157'
+
+
+# For development server
+
+
+class Spotify(object):
+    CLIENT_ID = 'c13f9174976545dc88adabebe21d7e56'
+    CLIENT_SECRET = '4ebe64f377994602b0ebf12232ab4d2b'
+
+
+INDEX = 0
+
+TRACK = 'track'
+ARTIST = 'artist'
+TRACK_ID = 'track_id'
+ID = 'id'
+OWNER = 'owner'
+NAME = 'name'
+ITEMS = 'items'
