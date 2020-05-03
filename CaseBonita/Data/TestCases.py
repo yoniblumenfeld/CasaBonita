4675d78f2a969341c36938b16fcd85d11f69fb94
yovelcohen <yovell04@gmail.com>
2020-05-03 14:54:28 +0300

    Test Cases

---
+++ CaseBonita/Data/TestCases.py
@@ -0,0 +1,7 @@
+class SpotifyTestCase:
+    USERNAME = 'oa7fxnn3xwxveug6000igp0v6'
+    SONGS_ID_LIST = ['7xGfFoTpQ2E7fRF5lN10tr']
+
+
+class AppleMusicTestCase:
+    PLAYLIST_EXAMPLE = "https://music.apple.com/il/playlist/shiras-18th-birthday/pl.u-9N9L24LIx3DZ3K0"
